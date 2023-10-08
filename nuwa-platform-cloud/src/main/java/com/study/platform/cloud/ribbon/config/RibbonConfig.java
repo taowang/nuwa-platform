835b172b50bf4a659036774a0fd8b999f66a2e4e
taowang <864734049@qq.com>
2023-10-08 22:49:10 +0800

    提交

--- nuwa-platform-cloud/src/main/java/com/study/platform/cloud/ribbon/config/RibbonConfig.java
+++ nuwa-platform-cloud/src/main/java/com/study/platform/cloud/ribbon/config/RibbonConfig.java
@@ -2,7 +2,6 @@ package com.study.platform.cloud.ribbon.config;
 
 
 import com.netflix.loadbalancer.IRule;
-import com.netflix.loadbalancer.RandomRule;
 import com.netflix.loadbalancer.RoundRobinRule;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -19,6 +18,7 @@ public class RibbonConfig {
      */
     @Bean
     public IRule rule(){
+        //轮询策略
         return new RoundRobinRule();
         //随机策略  从所有可用的提供者中随机选择一个
         //return new RandomRule();
---
+++ nuwa-platform-sentinel/src/main/java/com/study/platform/sentinel/config/SentinelAutoConfig.java
@@ -0,0 +1,7 @@
+//package com.study.platform.sentinel.config;
+//
+//import org.springframework.context.annotation.Configuration;
+//
+//@Configuration
+//public class SentinelAutoConfig {
+//}
