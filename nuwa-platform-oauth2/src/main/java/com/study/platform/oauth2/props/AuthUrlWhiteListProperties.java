b0ee2e0351c8b860447c973d55bb6a68dd721a34
taowang <864734049@qq.com>
2024-09-01 14:14:27 +0800

    admin 前提交

--- nuwa-platform-oauth2/src/main/java/com/study/platform/oauth2/props/AuthUrlWhiteListProperties.java
+++ nuwa-platform-oauth2/src/main/java/com/study/platform/oauth2/props/AuthUrlWhiteListProperties.java
@@ -14,10 +14,25 @@ import java.util.List;
 @ConfigurationProperties(prefix = "oauth-list")
 public class AuthUrlWhiteListProperties {
 
-    // 白名单配置，不需要鉴权的url
+    /**
+     * 静态文件白名单配置，不需要鉴权的url
+     * 请求经过oauth2时，需要把服务名去掉，这里的配置oauth2不会去掉前缀
+     */
+    private List<String> staticFiles;
+
+    /**
+     * 白名单配置，不需要鉴权的url
+     */
     private List<String> whiteUrls;
 
-    //需要鉴权的公共url配置，不需要单独给所有的角色增加的url
+    /**
+     * 需要鉴权的公共url配置，不需要单独给所有的角色增加的url
+     */
     private List<String> authUrls;
+
+    /**
+     * 网关放行，由认证中心进行认证的url，比如单点登录
+     */
+    private List<String> tokenUrls;
 
 }
